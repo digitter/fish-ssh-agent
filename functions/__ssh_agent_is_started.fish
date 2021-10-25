b99113a2d9f85c2296e4d61bfd062b947f9a0564
kazuki <matchingserver@gmail.com>
2021-10-26 01:59:54 +0900

    reuse socket for tmux

--- functions/__ssh_agent_is_started.fish
+++ functions/__ssh_agent_is_started.fish
@@ -1,14 +1,21 @@
 function __ssh_agent_is_started -d "check if ssh agent is already started"
-	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
-		source $SSH_ENV > /dev/null
-	end
+  if [ -S "$agent" ]
+    echo -e "agent socket already exists.\nReuse the running agent socket and reinitialize SSH_AUTH_SOCKET."
+    set -gx SSH_AUTH_SOCK $agent
+    return 0
+  end
 
-	if begin; test -z "$SSH_AGENT_PID"; and test -z "$SSH_CONNECTION"; end
-		return 1
-	end
+  if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+    source $SSH_ENV > /dev/null
+  end
 
-	ssh-add -l > /dev/null 2>&1
-	if test $status -eq 2
-		return 1
-	end
+  if begin; test -z "$SSH_AGENT_PID"; and test -z "$SSH_CONNECTION"; end
+    return 1
+  end
+
+  ssh-add -l > /dev/null 2>&1
+  if test $status -eq 2
+    return 1
+  end
 end
+
--- functions/__ssh_agent_start.fish
+++ functions/__ssh_agent_start.fish
@@ -2,4 +2,8 @@ function __ssh_agent_start -d "start a new ssh agent"
   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
   chmod 600 $SSH_ENV
   source $SSH_ENV > /dev/null
+
+  set agent "$HOME/.ssh/agent_tmp"
+  ln -snf "$SSH_AUTH_SOCK" $agent && set -gx SSH_AUTH_SOCK $agent
 end
+
